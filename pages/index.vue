73eb37ab3c57b28f03bb4521d8e77b519352696c
Marvin <mmarinduque.uc@gmail.com>
2023-09-13 04:18:30 +0800

    added sass and tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
+    <div>
+      something
+
+      <span>diha</span>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  div {
+    background:  red;
+    height: 200px;
+
+    span {
+      font-size: 20px;
+    }
+  }
+</style>
