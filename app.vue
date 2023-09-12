dd6eb605db884aa8319e56256e99c2c79ea85924
Marvin Marinduque <marinduquemarvin@gmail.com>
2023-09-13 01:11:10 +0800

    added pages and layouts

--- app.vue
+++ app.vue
@@ -1,5 +1,35 @@
+<script setup>
+useSeoMeta({
+  title: '[title]',
+  description: '[description]',
+  ogTitle: '[og:title]',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
