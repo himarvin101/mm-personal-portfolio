d3dc738a29cd1dc72f4b5f99f352455c2e52e5e5
Marvin Marinduque <marinduquemarvin@gmail.com>
2023-09-16 00:10:28 +0800

    new changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
   ],
+
+
+  // Uncomment when deploying
+  app: {
+    baseURL: '/mm-personal-portfolio/',
+    buildAssetsDir: 'assets',
+  }
 })
